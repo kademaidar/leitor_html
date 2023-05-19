aa34fbe9821595cb8b1447cc72df073776e01e31
Kadem Aidar <52976640+kademaidar@users.noreply.github.com>
2023-05-19 17:56:54 -0300

    Add files via upload

--- main_v2.0.py
+++ main_v2.0.py
@@ -12,48 +12,38 @@ def separa_tabela(arquivo_baixado):
     soup = BeautifulSoup(arquivo_baixado, 'html.parser')
 
     table = soup.find('table', id='jqGridTableDIVGrid')
-    identificador = soup.find('div', id='jqgh_jqGridTableDIVGrid_danfe')
 
     if table:
-        if identificador:  # Aqui ele entra se for NFe
-            
-            data = []
-            for row in table.find_all('tr'):
-                row_data = [cell.get_text(strip=True) for cell in row.find_all('td')]
-                if row_data:
-                    data.append(row_data)
 
-            df_NFe = pd.DataFrame(data)
-            df_NFe.drop(columns=[0, 1, 4, 5, 11, 17], inplace=True)  # Tem umas colunas que podem ser interessantes, perguntar se o usuario tem interesse
-            df_NFe.drop(index=0, inplace=True)
-            df_NFe.columns = ['Chave de Acesso', 'I.E. Dest.', 'I.E. Emit.', 'Razão Social Emit.',
+        data = []
+        for row in table.find_all('tr'):
+            row_data = [cell.get_text(strip=True) for cell in row.find_all('td')]
+            if row_data:
+                data.append(row_data)
+
+        df_temporario = pd.DataFrame(data)
+        df_temporario.drop(index=0, inplace=True)
+
+
+        if len(df_temporario.columns) == 18:  # Aqui ele entra se for NFe
+
+            df_temporario.drop(columns=[0, 1, 4, 5, 11, 17], inplace=True)  # Tem umas colunas que podem ser interessantes, perguntar se o usuario tem interesse
+            df_temporario.columns = ['Chave de Acesso.', 'I.E. Dest.', 'I.E. Emit.', 'Razão Social Emit.',
                                     'CNPJ/CPF Emit.', 'Nº NF', 'Dt. Emissão Fuso MS', 'UF Emit.', 'Total NF',
                                     'Base Cálc. ICMS', 'Valor ICMS', 'Sit.*']  # Sao 12 colunas
 
-            return df_NFe
+            return df_temporario
 
-        else:  # Aqui ele entra se for CTe
+        elif len(df_temporario.columns) == 12:  # Aqui ele entra se for CTe
 
-            data = []
-            for row in table.find_all('tr'):
-                row_data = [cell.get_text(strip=True) for cell in row.find_all('td')]
-                if row_data:
-                    data.append(row_data)
+            df_temporario.drop(columns=[0], inplace=True)  # Tem uma coluna que pode ser interessante, perguntar se o usuario tem interesse
+            df_temporario.columns = ['Chave de Acesso', 'Nome Empresa Emitente', 'CNPJ Emitente', 'Ano/Série/Número',
+                                'Data de Emissão', 'Data de Autorização', 'Número do Protocolo',
+                                'Valor Total da Prestação do Serviço', 'Valor da BC do ICMS',
+                                'Valor do ICMS', 'Sit']  # Sao 11 colunas
 
-            df_CTe = pd.DataFrame(data)
-            df_CTe.drop(columns=[0], inplace=True)  # Tem uma colunas que pode ser interessante, perguntar se o usuario tem interesse
-            df_CTe.drop(index=0, inplace=True)
-            df_CTe.columns = ['Chave de Acesso', 'Nome Empresa Emitente', 'CNPJ Emitente', 'Ano/Série/Número',
-                              'Data de Emissão', 'Data de Autorização', 'Número do Protocolo',
-                              'Valor Total da Prestação do Serviço', 'Valor da BC do ICMS',
-                              'Valor do ICMS', 'Sit']  # Sao 11 colunas
+            return df_temporario
 
-            return df_CTe
-
-
-
-    else:
-        return print('Tabela não encontrada no arquivo HTML.')
 
 
 def converte_formatos(df_entrada):
@@ -62,7 +52,7 @@ def converte_formatos(df_entrada):
 
     if len(df_entrada.columns) == 12:
         formatos_dados_NFe = {
-            'Chave de Acesso': 'string',
+            'Chave de Acesso.': 'string',
             'I.E. Dest.': 'string',
             'I.E. Emit.': 'string',
             'Razão Social Emit.': 'string',
@@ -85,12 +75,7 @@ def converte_formatos(df_entrada):
                               'Nome Empresa Emitente': 'string', 
                               'CNPJ Emitente': 'string', 
                               'Ano/Série/Número': 'string',
-                            #   'Data de Emissão', 
-                            #   'Data de Autorização', 
                               'Número do Protocolo': 'string',
-                            #   'Valor Total da Prestação do Serviço', 
-                            #   'Valor da BC do ICMS',
-                            #   'Valor do ICMS', 
                               'Sit': 'string',
         }
 
@@ -107,9 +92,10 @@ def converte_formatos(df_entrada):
 
 #### Codigo comeca a rodar aqui
 
-uploaded_htmls = st.file_uploader("Escolha arquivos HTML",
-                                  type=['html'],
-                                  accept_multiple_files=True)
+
+uploaded_htmls = st.file_uploader('Selecione os arquivos HTML',
+                                type=['html'],
+                                accept_multiple_files=True)
 
 
 if len(uploaded_htmls) == 0:
@@ -134,15 +120,22 @@ else:
 
     df_saida = pd.concat(lista_dfs)
 
+
+    if len(df_saida.columns) > 13:
+        st.warning('Cuidado: Há mais de um tipo de arquivo!')
+
+
     excel_saida = io.BytesIO()
     df_saida.to_excel(excel_saida, index=False, sheet_name='Tabela')
     excel_saida.seek(0)
 
+    
     st.write('Você colocou ' + str(len(uploaded_htmls)) + ' arquivos.')
     st.write("Nome dos arquivos: " + str(nome_arquivos))
     st.write("Número total de linhas: " + str(len(df_saida)))
 
+
     st.download_button('Baixar Excel',
-                       data=excel_saida,
-                       file_name='tabela_extraida.xlsx')
+                    data=excel_saida,
+                    file_name='tabela_extraida.xlsx')
     
